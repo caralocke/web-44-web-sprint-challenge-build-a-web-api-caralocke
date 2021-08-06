250f4a21100b973d043cc4214984a746a3dd6e18
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-06 06:39:25 -0400

    Created a few middleware functions and created endpoint for editing a project.

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,27 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+function validateProjectId(req, res, next) {
+    const { id } = req.params
+    Projects.get(id)
+        .then(project => {
+            if(project) {
+                next()
+            } else {
+                res.status(404).json({message :`Project with specified id of ${id} does not exist.`})
+            }
+        })
+}
+
+function validateProject(req, res, next) {
+    const { name, description, completed } = req.body
+    if(!name || !description || (completed == undefined)){
+        res.status(400).json({ message: 'Please provide a name, description, and the completed status of the project.'})
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    validateProjectId,
+    validateProject
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const Projects = require('./projects-model')
 const router = express.Router()
+const { validateProjectId, validateProject } = require('./projects-middleware')
 
 router.get('/', (req, res) => {
     Projects.get()
@@ -32,10 +33,7 @@ router.get('/:id', (req, res) => {
     });
 })
 
-router.post('/', (req, res) => {
-    if(!req.body.name || !req.body.description) {
-        res.status(400).json({ message: 'Please provide name and descrption'})
-    } else {
+router.post('/', validateProject, (req, res) => {
         const { name, description, completed } = req.body
         const { id } = Date.now()
         const newProject = { id, name, description, completed }
@@ -48,7 +46,20 @@ router.post('/', (req, res) => {
             message: err.message
         });
     });
-    }
+})
+
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(project => {
+            if(project) {
+                res.status(200).json(project)
+            } else {
+                res.status(404).json({ message: 'The project could not be found'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message})
+        })
 })
 
 module.exports = router
