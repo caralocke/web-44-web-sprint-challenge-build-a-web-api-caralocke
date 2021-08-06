a2a0a5e3766c36f1d93b691c86d7a850276299f0
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-05 20:38:09 -0400

    Completed writing endpoint for finding a project by id.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Projects.get(id)
+    .then(project => {
+      if(project) {
+        res.status(200).json(project)
+      } else {
+        res.status(404).json({
+          message: `Project with id of ${id} does not exist`
+	});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+})
+
 module.exports = router
