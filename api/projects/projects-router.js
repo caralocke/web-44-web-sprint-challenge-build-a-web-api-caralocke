2dd897a3b91572f1e61dc95eef63483adc8a80b9
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-06 06:45:59 -0400

    Created endpoint for deleting a project.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -62,4 +62,18 @@ router.put('/:id', validateProjectId, validateProject, (req, res) => {
         })
 })
 
+router.delete('/:id', validateProjectId, (req, res) => {
+    const { id } = req.params
+    Projects.get(id)
+        .then(project => {
+            Projects.remove(id)
+                .then(() => {
+                    res.status(200).json(project)
+                })
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message})
+        })
+})
+
 module.exports = router
