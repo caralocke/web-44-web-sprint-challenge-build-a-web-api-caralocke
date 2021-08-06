ed4288c552681c4642c8b3a39564d920a4609b9c
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-05 20:28:15 -0400

    Completed setting up endponit for getting projects.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message})
+        })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require('express');
 const server = express();
+const cors = require('cors')
+const projectsRouter = require('./projects/projects-router')
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
 
+server.use('/api/projects', projectsRouter)
 module.exports = server;
